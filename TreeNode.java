b72916ec5155bb0c13cb3271b0cda93b0dc2e5cb
María Sierra <sie24405@uvg.edu.gt>
2025-03-26 11:27:06 -0600

    Update TreeNode.java

--- TreeNode.java
+++ TreeNode.java
@@ -1,1 +1,12 @@
+public class TreeNode {
+    public String key; 
+    public Producto value; 
+    public TreeNode left, right; 
+
+    public TreeNode(String key, Producto value) {
+        this.key = key;
+        this.value = value;
+        this.left = this.right = null;
+    }
+}
 
