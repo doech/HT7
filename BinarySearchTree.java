61fbcd7ab83e76793481e5c5f33997d29dd60fd6
María Sierra <sie24405@uvg.edu.gt>
2025-03-26 13:52:23 -0600

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,1 +1,44 @@
+public class BinarySearchTree {
+    private TreeNode root;
+    public void insert(String key, Producto value) {
+        TreeNode newNode = new TreeNode(key, value);
+        if (root == null) {
+            root = newNode; 
+        } else {
+            TreeNode current = root;
+            TreeNode parent;
+            while (true) {
+                parent = current;
+                if (key.compareTo(current.key) < 0) { 
+                    current = current.left;
+                    if (current == null) {
+                        parent.left = newNode;
+                        return;
+                    }
+                } else { // Ir al subárbol derecho
+                    current = current.right;
+                    if (current == null) {
+                        parent.right = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public Producto search(String key) {
+        TreeNode current = root;
+        while (current != null) {
+            if (key.equals(current.key)) {
+                return current.value; 
+            } else if (key.compareTo(current.key) < 0) {
+                current = current.left; 
+            } else {
+                current = current.right;
+            }
+        }
+        
+        return null; 
+    }
+}
 
